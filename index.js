998901afbe6ed2f014c58ffaf5ad8ec4da69f884
Rodrigo Salinas <rodrigo.salinas@kotka.cl>
2020-04-12 14:03:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,83 @@
-module.exports = (sessionRef, opts) => {
-  const options = Object.assign({
-    property: 'session',
-    getSessionKey: (ctx) => ctx.from && ctx.chat && `${ctx.from.id}/${ctx.chat.id}`
-  }, opts)
+module.exports = (db, opts) => {
+    const options = Object.assign({
+        property: 'session',
+        getSessionKey: (ctx) => ctx.from && ctx.chat && `${ctx.from.id}/${ctx.chat.id}`
+    }, opts)
 
-  function getSession (key) {
-    return sessionRef.child(key).once('value')
-      .then((snapshot) => snapshot.val())
-  }
-
-  function saveSession (key, session) {
-    if (!session || Object.keys(session).length === 0) {
-      return sessionRef.child(key).remove()
+    function getSession(key) {
+        return new Promise(async(resolve, reject) => {
+            try {
+                let session = await getBotSessionByKey(db, key)
+                if (session) {
+                    resolve(JSON.parse(session.sessionValues))
+                } else {
+                    resolve(undefined)
+                }
+            } catch (err) {
+                console.error('getSession', err)
+                reject(err)
+            }
+        })
     }
-    return sessionRef.child(key).set(session)
-  }
 
-  return (ctx, next) => {
-    const key = options.getSessionKey(ctx)
-    if (!key) {
-      return next()
+    function saveSession (key, sessionValues) {
+        return new Promise(async(resolve, reject) => {
+            try {
+                let session = await getBotSessionByKey(db, key)
+                if ((!sessionValues || Object.keys(sessionValues).length === 0) && session) {
+                    await db.delete(db.key(['BotSession', parseInt(session[db.KEY].id)]))
+                } else if (session) {
+                    session.sessionValues = JSON.stringify(sessionValues)
+                    await db.update(session)
+                } else {
+                    if (sessionValues && Object.keys(sessionValues).length > 0) {
+                        session = {
+                            key: db.key('BotSession'),
+                            data: {
+                                key: key,
+                                sessionValues: JSON.stringify(sessionValues)
+                            }
+                        }
+                        await db.upsert(session)
+                    }
+                }
+                resolve()
+            } catch (err) {
+                console.error('saveSession', err)
+                reject(err)
+            }
+        })
     }
-    return getSession(key).then((value) => {
-      let session = value || {}
-      Object.defineProperty(ctx, options.property, {
-        get: function () { return session },
-        set: function (newValue) { session = Object.assign({}, newValue) }
-      })
-      return next().then(() => saveSession(key, session))
+
+    return (ctx, next) => {
+        const key = options.getSessionKey(ctx)
+        if (!key) {
+            return next()
+        }
+        return getSession(key).then((value) => {
+            let session = value || {}
+            Object.defineProperty(ctx, options.property, {
+                get: function () { return session },
+                set: function (newValue) { session = Object.assign({}, newValue) }
+            })
+            return next().then(() => saveSession(key, session))
+        })
+    }
+}
+
+function getBotSessionByKey(db, key) {
+    return new Promise(async(resolve, reject) => {
+        try {
+            const query    = await db.createQuery('BotSession').filter('key', '=', key)
+            let [sessions] = await db.runQuery(query)
+            if (sessions.length > 0) {
+                resolve(sessions[0])
+            } else {
+                resolve(undefined)
+            }
+        } catch (err) {
+            console.error('getBotSessionByKey', err)
+            reject(err)
+        }
     })
-  }
 }
